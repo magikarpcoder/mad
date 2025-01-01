cc13d1eaef7b58725bbb875c03f71db98439e298
magikarpcoder <91388772+magikarpcoder@users.noreply.github.com>
2025-01-02 02:18:06 +0530

    Update p1.java

--- p1/p1.java
+++ p1/p1.java
@@ -1,1 +1,11 @@
+package com.example.visitingcardapplication;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class MainActivity extends AppCompatActivity {
+ @Override
+ protected void onCreate(Bundle savedInstanceState) {
+ super.onCreate(savedInstanceState);
+ setContentView(R.layout.activity_main);
+ }
+}
 
