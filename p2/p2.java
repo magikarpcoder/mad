2207875331dd8b2e80f3784f9d2ef52cd88588ac
magikarpcoder <91388772+magikarpcoder@users.noreply.github.com>
2025-01-15 19:54:37 +0530

    Update p2.java

--- p2/p2.java
+++ p2/p2.java
@@ -1,5 +1,5 @@
-
 package com.example.wallpaperchangeapplication;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.app.WallpaperManager;
 import android.graphics.Bitmap;
@@ -11,56 +11,84 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.core.content.ContextCompat; // For ContextCompat.getDrawable
+
 import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
- Button wallpaperChange;
- Timer mytimer;
- Drawable drawable;
- WallpaperManager wpm;
- int prev=1;
- @Override
- protected void onCreate(Bundle savedInstanceState) {
- super.onCreate(savedInstanceState);
- setContentView(R.layout.activity_main);
- mytimer=new Timer();
- wpm = WallpaperManager.getInstance(this);
- wallpaperChange=(Button)findViewById(R.id.button1);
- wallpaperChange.setOnClickListener(new View.OnClickListener() {
- @Override public void onClick(View view) {
- setwallpaper();
- }
- });
- }
- private void setwallpaper() {
- Toast.makeText(this,"setting Wallpaper please wait.",Toast.LENGTH_LONG).show();
- mytimer.schedule(new TimerTask() {
- @Override
- public void run()
- {
- if(prev==1) {
- drawable = getResources().getDrawable(R.drawable.i1); prev = 2;
- }
- else if(prev==2) {
- drawable = getResources().getDrawable(R.drawable.i2); prev=3;
- }
-else if(prev==3) {
- drawable = getResources().getDrawable(R.drawable.i3); prev=4;
- }
- else if(prev==4) {
- drawable = getResources().getDrawable(R.drawable.i4); prev=5;
- }
- else if(prev==5) {
- drawable = getResources().getDrawable(R.drawable.i5); prev=1;
- }
- Bitmap wallpaper = ((BitmapDrawable)drawable).getBitmap(); try {
- wpm.setBitmap(wallpaper);
- }
- catch (IOException e)
- { e.printStackTrace();
- }
- }
- },0,30000);
- }
- }
+
+    Button wallpaperChange;
+    Timer mytimer;
+    WallpaperManager wpm;
+    int prev = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mytimer = new Timer();
+        wpm = WallpaperManager.getInstance(this);
+
+        wallpaperChange = findViewById(R.id.button1);
+        wallpaperChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setWallpaper();
+            }
+        });
+    }
+
+    private void setWallpaper() {
+        Toast.makeText(this, "Setting wallpaper, please wait.", Toast.LENGTH_LONG).show();
+
+        mytimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                // Get the drawable resource based on the current index
+                Drawable drawable;
+                if (prev == 1) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.one);
+                    prev = 2;
+                } else if (prev == 2) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.two);
+                    prev = 3;
+                } else if (prev == 3) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.three);
+                    prev = 4;
+                } else if (prev == 4) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.four);
+                    prev = 5;
+                } else if (prev == 5) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.five);
+                    prev = 1;
+                } else {
+                    return;
+                }
+
+                // Convert drawable to bitmap and set as wallpaper
+                if (drawable instanceof BitmapDrawable) {
+                    Bitmap wallpaper = ((BitmapDrawable) drawable).getBitmap();
+                    try {
+                        // Run wallpaper setting on the main thread
+                        new Handler(getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    wpm.setBitmap(wallpaper);
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, 0, 8000); // Set to change every 30 seconds
+    }
+}
